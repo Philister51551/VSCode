dbbdef189fab73a3835deef7da7aa6b1cc9337c5
Philister51551 <1026619787@qq.com>
2022-03-29 17:14:56 +0800

    add components demo.

--- Vue/my-project/src/App.vue
+++ Vue/my-project/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{msg}}</h1>
+    <firstcomponent></firstcomponent>
     <router-view/>
   </div>
 </template>
 
 <script>
+import firstcomponent from './components/firstcomponent.vue'
 export default {
   name: 'App',
   data () {
     return {
       msg: 'hello Vue!'
     }
-  }
+  },
+  components: {firstcomponent}
 }
 </script>
 
---
+++ Vue/my-project/src/components/firstcomponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="firstcomponent">
+    <a>written by {{author}}</a>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: 'wangzhi'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
