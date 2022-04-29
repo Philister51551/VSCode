5d9735ccb76e6ca252f9490c17d0e1702e0a7b96
Philister51551 <1026619787@qq.com>
2022-04-29 15:11:29 +0800

    add bind-ui

--- Vue/hello-world/src/main.ts
+++ Vue/hello-world/src/main.ts
@@ -4,4 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// createApp.prototype.msgTest = function msgTest() {
+//     this.alert("666")
+// }
+
 createApp(App).use(store).use(router).mount('#app')
--- Vue/hello-world/vue.config.js
+++ Vue/hello-world/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const { pathStr } = require('./src/settings.js')
+const defalutSettings = require('./src/settings.js')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: pathStr
 })
