0a592ba443709d5d49331b9386b0b71844ebcf23
Philister.work <1026619787@qq.com>
2022-05-07 17:12:08 +0800

    add 4 API test.

--- Vue/project-saas-hrm-vue-master/src/module-demo/pages/index.vue
+++ Vue/project-saas-hrm-vue-master/src/module-demo/pages/index.vue
@@ -32,6 +32,28 @@ const axiosDemo1 = axios.create({
   }
 })
 
+const axiosDemo2 = axios.create({
+  baseURL: '/api/api/orgs/6899/groups/4178/devices',
+  timeout: 5000,
+  headers: {
+    'token' : '32b0d1e410134134a69b98692b99c157',
+    //'token' : '57b46f2498e64231bbcc8379b7b90026',
+    'Content-Type': 'application/json; charset=utf-8'
+    //'Access-Control-Allow-Origin' : '*'
+  }
+})
+
+const axiosDemo3 = axios.create({
+  baseURL: '/api/api/orgs/6899/devicestate/15261',
+  timeout: 5000,
+  headers: {
+    'token' : '32b0d1e410134134a69b98692b99c157',
+    //'token' : '57b46f2498e64231bbcc8379b7b90026',
+    'Content-Type': 'application/json; charset=utf-8'
+    //'Access-Control-Allow-Origin' : '*'
+  }
+})
+
 import { list } from "@/api/example/table";
 import axios from "axios";
 export default {
@@ -51,10 +73,11 @@ export default {
       });
     },
     getData() {
-      axiosDemo1({
+      axiosDemo3({
         method: 'get'
       }).then((res) => {
-        this.tname=res.data.data[0];
+        //this.tname = res.data.data[0];
+        this.tname = res.data.data;
         console.log(res.data.data[0]);
       }).catch((err) => {
         console.log(err);
